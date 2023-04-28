1f880865f8e289400c04b49131fe0c38b559ceb3
96gen <96genJQ@gmail.com>
2023-04-28 20:25:46 +0800

    use @RequestParam pass id,firstName,lastName

--- src/main/java/com/restapi/Controller/StudentController.java
+++ src/main/java/com/restapi/Controller/StudentController.java
@@ -3,6 +3,7 @@ package com.restapi.Controller;
 import com.restapi.Entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -39,10 +40,25 @@ public class StudentController {
     //舉例來說輸入 http://localhost:8080/students/1/AAA/BBB
     //就能看到回傳的{"id":1,"firstName":"AAA","lastName":"BBB"}
     //透過網址傳入學生的ID、firstName、lastName
+    //使用@PathVariable讀取網址傳來的資料
     @GetMapping("/students/{id}/{firstName}/{lastName}")
     public Student studentPathVariable(@PathVariable("id") int studentId,
                                        @PathVariable("firstName") String firstName,
                                        @PathVariable("lastName") String lastName){
         return new Student(studentId, firstName, lastName);
+    }
+    //可以透過 http://localhost:8080/students/query?id=數字&firstName=名字&lastName=姓氏 來傳入資料
+    //舉例來說輸入 http://localhost:8080/students/query?id=1&firstName=AAA&lastName=BBB
+    //就能看到回傳的{"id":1,"firstName":"AAA","lastName":"BBB"}
+    //透過網址傳入學生的ID、firstName、lastName
+    //使用@RequestParam讀取網址傳來的資料
+    //和@PathVariable不同，傳入的順序不限定為「id再來firstName最後是lastName」
+    //換成http://localhost:8080/students/query?id=1&lastName=BBB&firstName=AAA
+    //還是能取得相同的結果
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
     }
 }
